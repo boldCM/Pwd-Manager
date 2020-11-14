32c5b735db8c1a6e12b785a312669e00ca83d000
boldCM <carolin@cc-meier.de>
2020-11-14 15:42:21 +0100

    more refractoring

--- App.js
+++ App.js
@@ -4,15 +4,18 @@ const { addEntry } = require("./lib/addEntry");
 const inquirer = require("inquirer");
 const { readCommandLineArguments } = require("./lib/commandline");
 const { validateSuperSavePassword } = require("./lib/validation");
-const { runQuestionForget } = require("./lib/questionForget");
-const { readPasswordSafe } = require("./lib/accesDB");
+const { runQuestionForget, runQuestionNewEntry } = require("./lib/questions");
+const { getPassword, readPasswordSafe } = require("./lib/accesDB");
 const Crypto = require("crypto-js");
 
 // Wie soll meine App aussehen?
 // 0unnötige Argumente nicht anzeigen
 // 1 Name der App
 // 2 Abfrage des Masterpasswordes
-// 3 Will ich ein Password haben oder einen neuen Eintrag hinzufügen?
+// 3 Will ich ein Password haben oder
+//    einen neuen Eintrag hinzufügen oder
+//    ein Password ändern oder
+//    einen Eintrag löschen
 // 4 entsprechende unterfunktionen anlegen...
 
 const args = readCommandLineArguments();
@@ -22,32 +25,27 @@ const userName = args[0];
 console.log(chalk.green("PWD-Manager"));
 console.log(chalk.green(`Hello ${userName} `));
 
-const questionNewEntry = {
-  type: "list",
-  name: "newEntry",
-  message: "Do you want to add a new entry?",
-  choices: ["yes", "no"],
-};
-
 async function validateAccess() {
   await validateSuperSavePassword();
 
-  const passwordName = await runQuestionForget();
+  // const passwordName = await runQuestionForget();
 
-  const passwordSafe = await readPasswordSafe(passwordName);
+  const passwordSafeRead = await readPasswordSafe();
 
-  if (passwordSafe[passwordName]) {
-    console.log(chalk.green(`Name: ${passwordSafe[passwordName].name}`));
-    console.log(chalk.green(`Password: ${passwordSafe[passwordName].pw}`));
+  // const passwordSafe = await getPassword(passwordName);
 
-    const { newEntry } = await inquirer.prompt(questionNewEntry);
-    if (newEntry === "yes") {
-      addEntry(passwordSafe);
-    } else {
-      return;
-    }
+  // if (passwordSafe[passwordName]) {
+  //   console.log(chalk.green(`Name: ${passwordSafe[passwordName].name}`));
+  //   console.log(chalk.green(`Password: ${passwordSafe[passwordName].pw}`));
+
+  const newEntry = await runQuestionNewEntry();
+  if (newEntry === "yes") {
+    await addEntry(passwordSafeRead);
   } else {
-    console.log(chalk.yellow("Unknown Password"));
+    return;
   }
+  //  else {
+  //   console.log(chalk.yellow("Unknown Password"));
+  // }
 }
 validateAccess();
